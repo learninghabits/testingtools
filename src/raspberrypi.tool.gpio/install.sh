77ff7299939737aa30a705ff7b8c6a056dbc3d7c
Tshepo Ntlhokoa <tshepo.ntlhokoa@entelect.co.za>
2025-09-24 19:08:09 +0200

    bug fixes

--- src/raspberrypi.tool.gpio/install.sh
+++ src/raspberrypi.tool.gpio/install.sh
@@ -5,6 +5,6 @@ sudo apt-get update --fix-missing && apt-get install -y wget tar \
     && wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh \
     && chmod +x dotnet-install.sh
 
-# Execute the script in a separate RUN command
-sudo    && rm dotnet-install.sh
-    
+# Install .NET 8 runtime and clean up
+./dotnet-install.sh --runtime dotnet --version 8.0.0
+rm dotnet-install.sh
